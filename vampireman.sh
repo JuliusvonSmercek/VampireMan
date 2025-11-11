5f0019113be20b4dbe42983d516658c8fcd3adb5
JohannaHn <st180465@stud.uni-stuttgart.de>
2025-11-11 08:44:54 +0100

    update run scripts

--- vampireman.sh
+++ vampireman.sh
@@ -9,7 +9,7 @@ export LD_LIBRARY_PATH=$PETSC_DIR/$PETSC_ARCH/lib:$LD_LIBRARY_PATH
 export PATH=$PETSC_DIR/$PETSC_ARCH/bin:$PATH
 alias mpirun='mpirun -x LD_LIBRARY_PATH'
 case="case35_only_cooling"
-dataset_out="case_35_only_cooling-4"
+dataset_out="case_35_only_cooling-5"
 mkdir -p ./datasets_out/${dataset_out}
 cp ./settings/${case}.yaml ./datasets_out/${dataset_out}/settings.yaml
 python -m vampireman --non-interactive --settings-file ./settings/${case}.yaml && \
