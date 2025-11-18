8a1aafa864a6ca6826f888c2491ce6c75bc0e44f
JuliusvonSmercek <juliusvonsmercek@protonmail.com>
2025-11-19 00:52:18 +0100

    fix tqdm

--- vampireman/pflotran/simulation_stage.py
+++ vampireman/pflotran/simulation_stage.py
@@ -16,13 +16,6 @@ def run_simulation(datapoint_path, state):
   """
   stdout_file_path = os.path.join(datapoint_path, "simulation_stdout.log")
   stderr_file_path = os.path.join(datapoint_path, "simulation_stderr.log")
-  pflotran_out_path = os.path.join(datapoint_path, "pflotran.out")
-  pflotran_h5_path = os.path.join(datapoint_path, "pflotran.h5")
-
-  if state.general.interactive and (os.path.isfile(pflotran_out_path) and os.path.isfile(pflotran_h5_path)):
-    logging.warning(f"pflotran.out and pflotran.h5 files present in {datapoint_path}")
-    if not get_answer(state, "Looks like the simulation already ran, run simulation again?"):
-      return
 
   command: list[str] = []
   if state.general.mpirun:
@@ -52,6 +45,7 @@ def run_simulation(datapoint_path, state):
           current_time = float(match.group(1))
           progress_bar.n = min(current_time, progress_bar.total)
           progress_bar.refresh()
+          stdout_file.flush()
       process.wait()
       if process.returncode != 0:
         raise subprocess.CalledProcessError(process.returncode, command)
@@ -73,10 +67,17 @@ def simulation_stage(state: State):
   `vampireman.data_structures.GeneralConfig.mpirun`.
   """
 
-  datapoint_paths = [
-    state.general.output_directory / f"datapoint-{index}"
-    for index in range(state.general.number_datapoints)
-  ]
+  datapoint_paths = []
+  for index in range(state.general.number_datapoints):
+    datapoint_path = state.general.output_directory / f"datapoint-{index}"
+    pflotran_out_path = os.path.join(datapoint_path, "pflotran.out")
+    pflotran_h5_path = os.path.join(datapoint_path, "pflotran.h5")
+    make_simulation = True
+    if os.path.isfile(pflotran_out_path) and os.path.isfile(pflotran_h5_path):
+      logging.warning(f"pflotran.out and pflotran.h5 files present in {datapoint_path}")
+      make_simulation = get_answer(state, "Looks like the simulation already ran, run simulation again?")
+    if make_simulation:
+      datapoint_paths.append(datapoint_path)
 
   with ThreadPoolExecutor() as executor:
     futures = {
